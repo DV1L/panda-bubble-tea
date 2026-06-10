8ca8e191d7827ae6fdfb5bfc4e9352b476d86234
DV1L <hdavidl0321@gmail.com>
2026-06-09 19:03:53 -0600

    🧹 chore: clean project structure and routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { resolve } from 'node:path'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/panda-bubble-tea/',
 
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        menu: resolve(__dirname, 'menu.html')
+        main: resolve('index.html'),
+        menu: resolve('menu.html'),
+        checkout: resolve('checkout.html')
       }
     }
   }
